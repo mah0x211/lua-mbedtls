0ba2b11345a6dc6c59710b1a52e5c8f64c59bfb1
Masatoshi Teruya <mah0x211@gmail.com>
2016-05-19 21:28:02 +0900

    Apply a lauxhlib.h

--- src/hash.c
+++ src/hash.c
@@ -2,10 +2,10 @@
  *  Copyright (C) 2016 Masatoshi Teruya
  *
  *  Permission is hereby granted, free of charge, to any person obtaining a
- *  copy of this software and associated documentation files (the "Software"), 
- *  to deal in the Software without restriction, including without limitation 
- *  the rights to use, copy, modify, merge, publish, distribute, sublicense, 
- *  and/or sell copies of the Software, and to permit persons to whom the 
+ *  copy of this software and associated documentation files (the "Software"),
+ *  to deal in the Software without restriction, including without limitation
+ *  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ *  and/or sell copies of the Software, and to permit persons to whom the
  *  Software is furnished to do so, subject to the following conditions:
  *
  *  The above copyright notice and this permission notice shall be included in
@@ -15,8 +15,8 @@
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
  *  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
- *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ *  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  *  DEALINGS IN THE SOFTWARE.
  *
  *  src/hash.c
@@ -37,7 +37,7 @@
 
 #define hash_lua( L, hash_type, hash_api, digest, dlen, ... ) do{ \
     size_t ilen = 0; \
-    const char *input = lau_checklstring( (L), 1, &ilen ); \
+    const char *input = lauxh_checklstring( (L), 1, &ilen ); \
     /* check arguments */ \
     if( lua_isnoneornil( (L), 2 ) ){ \
         hash_api( (const unsigned char*)input, ilen, (digest), ##__VA_ARGS__ ); \
@@ -46,7 +46,7 @@
     } \
     else { \
         size_t klen = 0; \
-        unsigned char *key = (unsigned char*)lau_checklstring( L, 2, &klen ); \
+        unsigned char *key = (unsigned char*)lauxh_checklstring( L, 2, &klen ); \
         const mbedtls_md_info_t *info = mbedtls_md_info_from_type( hash_type ); \
         if( mbedtls_md_hmac( info, key, klen, (const unsigned char*)input, \
                              ilen, (digest) ) == 0 ){ \
@@ -122,7 +122,7 @@ LUALIB_API int luaopen_mbedtls_hash( lua_State *L )
     // create table
     lua_newtable( L );
     while( ptr->name ){
-        lau_fn2tbl( L, ptr->name, ptr->func );
+        lauxh_pushfn2tbl( L, ptr->name, ptr->func );
         ptr++;
     }
 
